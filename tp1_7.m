68e56ca904108bd274f0eacc3a191de28165f2c9
Diego Tabares <dtabares@gmail.com>
2019-05-03 20:31:50 -0300

    Agrego TP1 punto 7

---
+++ tp1_7.m
@@ -0,0 +1,12 @@
+function f = tp1_7(amplitude, frec_hz, init_phase, fs)
+
+% 1 segundo de duraction (pedido en el enunciado)
+% duration = number_of_samples/fs;
+% 1 * fs = number_of_samples;
+% fs = number_of_samples;
+
+x = 0:(1/fs):1;
+
+y = amplitude * cos((frec_hz * x) + init_phase);
+plot(x,y), grid on
+end
