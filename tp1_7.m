e88ba2d61f16ce75663482ffdaeca052bed0ca4b
Diego Tabares <dtabares@gmail.com>
2019-05-03 20:58:18 -0300

    agrego leyenda

--- tp1_7.m
+++ tp1_7.m
@@ -8,5 +8,9 @@ function f = tp1_7(amplitude, frec_hz, init_phase, fs)
 x = 0:(1/fs):1;
 
 y = amplitude * cos((frec_hz * x) + init_phase);
+func_text = (['y = ', num2str(amplitude), ' * cos( ' , num2str(frec_hz), ' * x + ', num2str(init_phase),' )' ]);
 plot(x,y), grid on
+legend({func_text},'Location','southwest')
+
+
 end
