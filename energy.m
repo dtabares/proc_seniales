dc15b50775d7116145aa560fc7e24548e406cf63
Diego Tabares <dtabares@gmail.com>
2019-05-06 23:04:43 -0300

    mejoro el calculo de la energia de una señal

--- energy.m
+++ energy.m
@@ -1,3 +1,6 @@
-function e = energy(signal)    
-    e = sum(abs(signal.^2));
+function e = energy(signal,fs,from,to)
+    if (nargin > 2)
+        signal = signal(from:to);
+    end   
+    e = sum(abs(signal.^2))/fs;
 end
