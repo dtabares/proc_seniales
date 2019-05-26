a5b1796aacb4e497b03c17699e76accde0f2610f
Diego Tabares <dtabares@gmail.com>
2019-05-26 20:08:27 -0300

    arreglo la energia de la senal continua y el punto 4

--- energy_cont.m
+++ energy_cont.m
@@ -1,6 +1,10 @@
 function e = energy_cont(signal, fs)
-%     f = @(t) abs(signal(t).^2);
-%     e = integral(f,-Inf,Inf);
-    d = calculate_duration(length(signal),fs);
-    e = integral(signal,0,d);
+    d = calculate_duration(fs,length(signal));
+    e = 0;
+    delta = 1/fs;
+    for value = signal
+        base = delta;
+        altura = value^2;
+        e = e + (base * altura);
+    end
 end
